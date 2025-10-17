5da2ee3473b9c50304bcacd032c789b3f1fa96f2
francisco.segado <francisco.segado@ext.thepowermba.com>
2025-10-17 12:55:34 +0200

    add exception examples

--- 01_clases/src/ApuntesClase.java
+++ 01_clases/src/ApuntesClase.java
@@ -1,7 +1,24 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ApuntesClase {
-    static void main(String[] args) {
+    static void main(String[] args) throws MiExcepcion {
+        prueba();
+    }
+
+    public static void prueba() throws MiExcepcion{
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese su edad");
+        int edad = sc.nextInt();
+        if (edad < 18) {
+            throw new MiExcepcion("Eres menor de edad");
+        }
 
     }
+
+    public static class MiExcepcion extends Exception {
+        public MiExcepcion(String mensaje) {
+            super( mensaje );
+        }
+    }
 }
+
