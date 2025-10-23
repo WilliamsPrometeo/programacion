83061a25e3a0f2ffe5bd2acf5aa7a54d79f2ff5c
WilliamsPrometeo <williams.infanzon@ext.thepower.education>
2025-10-23 08:52:33 +0200

    add changes on Persona

--- 02_ejercicios/src/clases/Persona.java
+++ 02_ejercicios/src/clases/Persona.java
@@ -5,14 +5,17 @@ public class Persona {
     private String apellido;
     private int edad;
     private Direccion direccion;
+    private int dinero_en_banco;
 
     public Persona() {
+        this.dinero_en_banco = 1000;
     }
 
     public Persona(String nombre, String apellido, int edad) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;
+        this.dinero_en_banco = 1000;
     }
 
     public String getNombre() {
@@ -47,12 +50,21 @@ public class Persona {
         this.direccion = direccion;
     }
 
+    public int getDinero_en_banco() {
+        return dinero_en_banco;
+    }
+
+    public void setDinero_en_banco(int dinero_en_banco) {
+        this.dinero_en_banco = dinero_en_banco;
+    }
+
     @Override
     public String toString() {
         return "Persona:\n" +
                 "\tNombre: '" + nombre + "'\n" +
                 "\tApellido: '" + apellido + "'\n" +
                 "\tEdad: " + edad + '\n' +
-                direccion + '\n';
+                direccion + '\n' +
+                "\tDinero en banco: " + dinero_en_banco;
     }
 }
