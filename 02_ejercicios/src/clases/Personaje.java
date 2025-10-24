c858ccb1a4d2bc681671e31c0de7df6b1470d7aa
WilliamsPrometeo <williams.infanzon@ext.thepower.education>
2025-10-24 11:36:17 +0200

    add game

--- 02_ejercicios/src/clases/Personaje.java
+++ 02_ejercicios/src/clases/Personaje.java
@@ -2,14 +2,30 @@ package clases;
 
 public class Personaje {
 
+    private String nombre;
     private int vida;
     private int ataque;
     private int defensa;
+
+    public Personaje(String nombre, int vida, int ataque, int defensa) {
+        this.nombre = nombre;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defensa = defensa;
+    }
 
     public Personaje(int vida, int ataque, int defensa) {
         this.vida = vida;
         this.ataque = ataque;
         this.defensa = defensa;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public int getVida() {
@@ -34,5 +50,13 @@ public class Personaje {
 
     public void setDefensa(int defensa) {
         this.defensa = defensa;
+    }
+
+    @Override
+    public String toString() {
+        return "Personaje: " + nombre +
+                ", vida: " + vida +
+                ", ataque: " + ataque +
+                ", defensa: " + defensa;
     }
 }
