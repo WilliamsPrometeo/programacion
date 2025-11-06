6508fcca70b1a41fd4d9184c751b858af083cf4a
WilliamsPrometeo <williams.infanzon@ext.thepower.education>
2025-11-06 10:24:42 +0100

    Document class

--- 02_ejercicios/src/main/java/ejemplos_clase/herencia/clases/Coche.java
+++ 02_ejercicios/src/main/java/ejemplos_clase/herencia/clases/Coche.java
@@ -2,32 +2,71 @@ package ejemplos_clase.herencia.clases;
 
 import ejemplos_clase.herencia.enums.Marca;
 
+/**
+ * Clase Coche que hereda de la clase abstracta Vehículo
+ * @see Vehiculo
+ * @author Profesor - Williams
+ * @version 1.0
+ */
 public class Coche extends Vehiculo {
 
     private int cv;
 
+    /**
+     * Construcutor principal de la clase Coche
+     * @see Vehiculo
+     * @param marca atributo heredado de Vehiculo
+     * @param modelo atributo heredado de Vehiculo
+     * @param num_ruedas atributo heredado de Vehiculo
+     * @param cv numero de caballos del coche
+     */
     public Coche(Marca marca, String modelo, int num_ruedas, int cv) {
         super(marca, modelo, num_ruedas);
         this.cv = cv;
     }
 
+    /**
+     * Constructor secundario de la clase Coche
+     * @see Vehiculo
+     * @param modelo atributo heredado de Vehiculo
+     * @param num_ruedas atributo heredado de Vehiculo
+     * @param cv numero de caballos del coche
+     */
     public Coche(String modelo, int num_ruedas, int cv) {
         super(modelo, num_ruedas);
         this.cv = cv;
     }
 
+    /**
+     * Constructor heredado de la clase Vehiculo
+     * @see Vehiculo
+     * @param modelo atributo heredado de Vehiculo
+     * @param num_ruedas atributo heredado de Vehiculo
+     */
     public Coche(String modelo, int num_ruedas) {
         super(modelo, num_ruedas);
     }
 
+    /**
+     * Getter del atributo Cv
+     * @return el número de caballos del coche
+     */
     public int getCv() {
         return cv;
     }
 
+    /**
+     * Setter del atributo Cv
+     * @param cv número de caballos del coche
+     */
     public void setCv(int cv) {
         this.cv = cv;
     }
 
+    /**
+     * Metodo sobreescrito de la clase heredada para mostrar los datos del coche
+     * @return datos del coche
+     */
     @Override
     public String mostrar() {
         return "Esto es un coche: " + super.getMarca() + super.getModelo() + this.getCv();
--- 02_ejercicios/src/main/java/ejemplos_clase/herencia/clases/Vehiculo.java
+++ 02_ejercicios/src/main/java/ejemplos_clase/herencia/clases/Vehiculo.java
@@ -2,45 +2,89 @@ package ejemplos_clase.herencia.clases;
 
 import ejemplos_clase.herencia.enums.Marca;
 
+/**
+ * Clase abstracta Vehiculo, para uso de herencia
+ * @author Profesor - Williams
+ * @version 1.0
+ */
 abstract class Vehiculo {
     private Marca marca;
     private String modelo;
     private int num_ruedas;
 
+    /**
+     * Constructor principal de la clase Vehículo
+     * @param marca Enumeración para establecer la marca
+     * @param modelo Cadena de caracteres para contener el modelo
+     * @param num_ruedas Numero de ruedas del vehiculo
+     */
     public Vehiculo(Marca marca, String modelo, int num_ruedas) {
         this.marca = marca;
         this.modelo = modelo;
         this.num_ruedas = num_ruedas;
     }
 
+    /**
+     * Constructor secundario de la clase Vehículo
+     * @param modelo Cadena de caracteres para contener el modelo
+     * @param num_ruedas Numero de ruedas del vehiculo
+     */
     public Vehiculo(String modelo, int num_ruedas) {
         this.modelo = modelo;
         this.num_ruedas = num_ruedas;
     }
 
+    /**
+     * Getter del atributo Marca
+     * @return la marca
+     */
     public Marca getMarca() {
         return marca;
     }
 
+    /**
+     * Setter del atributo Marca
+     * @param marca Enumeración para establecer la marca
+     */
     public void setMarca(Marca marca) {
         this.marca = marca;
     }
 
+    /**
+     * Getter del atributo Modelo
+     * @return el modelo
+     */
     public String getModelo() {
         return modelo;
     }
 
+    /**
+     * Setter del atributo Modelo
+     * @param modelo Cadena de caracteres para contener el modelo
+     */
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
 
+    /**
+     * Getter del atributo NumRuedas
+     * @return el número de ruedas
+     */
     public int getNum_ruedas() {
         return num_ruedas;
     }
 
+    /**
+     * Setter del atributo NumRuedas
+     * @param num_ruedas Número de ruedas del vehículo
+     */
     public void setNum_ruedas(int num_ruedas) {
         this.num_ruedas = num_ruedas;
     }
 
+    /**
+     * Metodo abstracto para mostrar los datos del vehículo
+     * @return los datos del vehículo
+     */
     public abstract String mostrar();
 }
