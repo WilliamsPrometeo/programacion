2030f446622f8748f6cfe6f00d1365e8e7368ac8
WilliasmPrometeo <williams.infanzon@ext.thepower.education>
2025-11-11 01:04:44 +0100

    Add JavaDoc

--- 02_ejercicios/src/main/java/ejemplos_clase/herencia/clases/Empleado.java
+++ 02_ejercicios/src/main/java/ejemplos_clase/herencia/clases/Empleado.java
@@ -5,41 +5,85 @@ import java.time.format.DateTimeFormatter;
 
 /**
  * Clase Empleado que hereda de la clase Persona
- * @see Persona
+ *
  * @author Profesor - Williams
  * @version 1.0
+ * @see Persona
  */
 public class Empleado extends Persona {
     private int num_empleado;
     private LocalDateTime fecha_alta;
 
+    /**
+     * Constructor heredado de la clase Persona
+     * Inicializamos el atributo fecha_alta con la fecha del momento de la creación
+     *
+     * @param nombre   atributo heredado
+     * @param apellido atributo heredado
+     * @param edad     atributo heredado
+     * @see Persona
+     */
     public Empleado(String nombre, String apellido, int edad) {
         super(nombre, apellido, edad);
         this.fecha_alta = LocalDateTime.now();
     }
 
+    /**
+     * Constructor principal de la clase Empleado
+     * Inicializamos el atributo fecha_alta con la fecha del momento de la creación
+     *
+     * @param nombre atributo heredado
+     * @param apellido atributo heredado
+     * @param edad atributo heredado
+     * @param num_empleado número identificativo del empleado
+     */
     public Empleado(String nombre, String apellido, int edad, int num_empleado) {
         super(nombre, apellido, edad);
         this.num_empleado = num_empleado;
         this.fecha_alta = LocalDateTime.now();
     }
 
+    /**
+     * Getter del atributo Num_Empleado
+     *
+     * @return el número identificativo del empleado
+     */
     public int getNum_empleado() {
         return num_empleado;
     }
 
+    /**
+     * Setter del atributo Num_Empleado
+     *
+     * @param num_empleado establece el número identificativo del empleado
+     */
     public void setNum_empleado(int num_empleado) {
         this.num_empleado = num_empleado;
     }
 
+    /**
+     * Getter del atributo Fecha_Alta
+     *
+     * @return la fecha de alta del empleado
+     */
     public LocalDateTime getFecha_alta() {
         return fecha_alta;
     }
 
+    /**
+     * Setter del atributo Fecha_Alta
+     *
+     * @param fecha_alta establece la fecha de alta del empleado
+     */
     public void setFecha_alta(LocalDateTime fecha_alta) {
         this.fecha_alta = fecha_alta;
     }
 
+    /**
+     * Metodo toString para mostrar los datos del empleado
+     *
+     * @return texto formateado con los datos del empleado
+     */
     @Override
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
--- 02_ejercicios/src/main/java/ejemplos_clase/herencia/clases/Persona.java
+++ 02_ejercicios/src/main/java/ejemplos_clase/herencia/clases/Persona.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 /**
  * Clase Persona
+ *
  * @author Profesor - Williams
  * @version 1.0
  */
@@ -13,10 +14,22 @@ public class Persona {
     private int edad;
     private ArrayList<Vehiculo> vehiculos;
 
+    /**
+     * Constructor vacio de la clase Persona
+     * Se inicializa la lista de vehiculos
+     */
     public Persona() {
         vehiculos = new ArrayList<>();
     }
 
+    /**
+     * Constructor principal de la clase Persona
+     * Se inicializa la lista de vehiculos
+     *
+     * @param nombre   Nombre de la persona
+     * @param apellido Apellido de la persona
+     * @param edad     Edad de la persona
+     */
     public Persona(String nombre, String apellido, int edad) {
         this.nombre = nombre;
         this.apellido = apellido;
@@ -24,38 +37,83 @@ public class Persona {
         this.vehiculos = new ArrayList<>();
     }
 
+    /**
+     * Getter del atributo Nombre
+     *
+     * @return el nombre de la persona
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Setter del atribtuo Nombre
+     *
+     * @param nombre estable el nombre de la persona
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Getter del atributo Apellido
+     *
+     * @return el apellido de la persona
+     */
     public String getApellido() {
         return apellido;
     }
 
+    /**
+     * Setter del atributo Apellido
+     *
+     * @param apellido establece el apellido de la persona
+     */
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
 
+    /**
+     * Getter del atributo Edad
+     *
+     * @return la edad de la persona
+     */
     public int getEdad() {
         return edad;
     }
 
+    /**
+     * Setter del atributo Edad
+     *
+     * @param edad establece la edad de la persona
+     */
     public void setEdad(int edad) {
         this.edad = edad;
     }
 
+    /**
+     * Getter del atributo vehiculos
+     *
+     * @return la lista de vehiculos de la persona
+     */
     public ArrayList<Vehiculo> getVehiculos() {
         return vehiculos;
     }
 
+    /**
+     * Setter del atributo vehiculos
+     *
+     * @param vehiculos establece la lista de vehiculos de la persona
+     */
     public void setVehiculos(ArrayList<Vehiculo> vehiculos) {
         this.vehiculos = vehiculos;
     }
 
+    /**
+     * Metodo toString para mostrar los datos de la persona
+     *
+     * @return texto formateado con los datos de la persona
+     */
     @Override
     public String toString() {
         return String.format("Datos de la persona: %s, %s, %d", this.nombre, this.apellido, this.edad);
