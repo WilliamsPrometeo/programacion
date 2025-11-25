a62d898a51d3ebbebb1c704b82ea1a4efef22449
WilliasmPrometeo <williams.infanzon@ext.thepower.education>
2025-11-26 00:15:13 +0100

    Add examen sorpresa análisis

--- 02_ejercicios/src/main/java/ejemplos_clase/Main.java
+++ 02_ejercicios/src/main/java/ejemplos_clase/Main.java
@@ -6,6 +6,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class Main {
@@ -31,9 +33,22 @@ public class Main {
 //        System.out.println(formato2);
 //        System.out.println(formato3);
 
+//        Random rand = new Random();
+//        int alumno = rand.nextInt(28 + 1);
+//        System.out.println("Alumno: " + alumno);
+
         Random rand = new Random();
-        int alumno = rand.nextInt(28 + 1);
-        System.out.println("Alumno: " + alumno);
+
+        System.out.println("=== ASIGNACIÓN DE VALORES PARA AnalisisComplejo ===\n");
+
+        // Asignación a 28 alumnos
+        for (int i = 0; i < 28; i++) {
+
+            // Generar número aleatorio entre 3 y 15 (ambos inclusive)
+            int valorAsignado = rand.nextInt(13) + 3;
+
+            System.out.printf("Alumno %02d → numero = %d%n", (i + 1), valorAsignado);
+        }
     }
 
 }
