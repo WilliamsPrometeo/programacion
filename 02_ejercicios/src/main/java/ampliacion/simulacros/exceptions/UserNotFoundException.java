6b95d29b161d49eec31020d3d51b358c53256165
WilliamsPrometeo <williams.infanzon@ext.thepower.education>
2025-12-04 11:31:22 +0100

    commit MOP 04/12

---
+++ 02_ejercicios/src/main/java/ampliacion/simulacros/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package ampliacion.simulacros.exceptions;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
