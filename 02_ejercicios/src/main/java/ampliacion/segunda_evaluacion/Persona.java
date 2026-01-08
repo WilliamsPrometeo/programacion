cafab686d5612540eeff247ffb0e02ba706b5f63
WilliamsPrometeo <williams.infanzon@ext.thepower.education>
2026-01-08 11:14:22 +0100

    commit MOP 08/01/26

---
+++ 02_ejercicios/src/main/java/ampliacion/segunda_evaluacion/Persona.java
@@ -0,0 +1,61 @@
+package ampliacion.segunda_evaluacion;
+
+public class Persona {
+    private String dni;
+    private  String nombre;
+    private String apellido;
+
+    public Persona(String dni, String nombre, String apellido) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+        if(obj == null || getClass() != obj.getClass()) return false;
+
+        Persona persona = (Persona) obj;
+
+        return this.dni != null ? this.dni.equals(persona.dni) : persona.dni == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return dni != null ? dni.hashCode() : 0;
+    }
+}
