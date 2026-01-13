99828af5b00d0df794205b3d3b08be2b83cd8077
WilliasmPrometeo <williams.infanzon@ext.thepower.education>
2026-01-13 23:40:46 +0100

    commit 13/01 preparación

--- 02_ejercicios/src/main/java/segunda_evaluacion/Main.java
+++ 02_ejercicios/src/main/java/segunda_evaluacion/Main.java
@@ -7,49 +7,62 @@ import java.io.*;
 public class Main {
 
     private static final MyScanner sc = new MyScanner();
+    private static final String RUTA = "02_ejercicios/datos/";
 
     public static void main(String[] args) {
 
-//        try (FileReader fr = new FileReader("texto.txt")){
-//            int caracter;
-//            while ((caracter = fr.read()) != -1) {
-//                System.out.print((char)caracter);
-//            }
-//        } catch (IOException e) {
-//            System.out.println("Error al abrir archivo");
-//        }
-//
-//        try(FileWriter fw = new FileWriter("otro.txt")) {
-//            String texto = "Estoy escribiendo en un archivo de texto";
-//            fw.write(texto);
-//        } catch (IOException e) {
-//            System.out.println("Error al escribir en el archivo");
-//        }
+        File origen = new File(RUTA + "images.jpg");
+        File destino = new File(RUTA + "otra.jpg");
 
-//        try (
-//            FileReader fr = new FileReader("texto.txt");
-//            FileWriter fw = new FileWriter("otro.txt");
-//            ){
-//            StringBuilder texto = new StringBuilder();
-//            int caracter;
-//            while ((caracter = fr.read()) != -1) {
-//                texto.append((char) caracter);
-//            }
-//            fw.write(texto.toString());
-//        } catch (IOException e) {
-//            System.out.println("ERROR");
-//        }
+        if(!origen.exists()) {
+            System.out.println("El archivo no existe");
+        } else {
+            copiar_archivo(origen, destino);
+        }
 
+        File origenTexto = new File(RUTA + "texto.txt");
+        File destinoTexto = new File(RUTA + "copia.txt");
+
+        if (!origenTexto.exists()) {
+            System.out.println("El archivo no existe");
+        } else {
+            copiarTexto(origenTexto, destinoTexto);
+        }
+
+    }
+
+    private static void copiar_archivo(File origen, File destino) {
         try (
-            FileInputStream fis = new FileInputStream("images.jpg");
-            FileOutputStream fos = new FileOutputStream("otraimagen.jpg");
+                FileInputStream fis = new FileInputStream(origen);
+                FileOutputStream fos = new FileOutputStream(destino);
         ) {
             int bytes;
             while ((bytes = fis.read()) != -1) {
                 fos.write(bytes);
             }
+
+            System.out.println("Archivo copiado correctamente");
         } catch (IOException e) {
-            System.out.println("ERROR");
+            System.out.println("ERROR " + e.getMessage());
         }
     }
+
+    public static void copiarTexto(File origen, File destino) {
+
+        try (
+                FileReader fr = new FileReader(origen);
+                FileWriter fw = new FileWriter(destino)
+        ) {
+            int caracter;
+            while ((caracter = fr.read()) != -1) {
+                fw.write(caracter);
+            }
+
+            System.out.println("Texto copiado correctamente.");
+
+        } catch (IOException e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+    }
+
 }
