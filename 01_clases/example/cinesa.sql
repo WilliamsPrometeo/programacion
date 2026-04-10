2fcaac3493fff397eb9eb1e924990e14d593fb72
WilliamsPrometeo <williams.infanzon@ext.thepower.education>
2026-04-10 08:38:43 +0200

    commit 10/04/26

---
+++ 01_clases/example/cinesa.sql
@@ -0,0 +1,32 @@
+ZCREATE TABLE usuarios(
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	dni VARCHAR(9) UNIQUE NOT NULL,
+	nombre VARCHAR(100) NOT NULL,
+	email VARCHAR(150) UNIQUE NOT NULL,
+	password VARCHAR(55) NOT NULL
+);
+
+CREATE TABLE peliculas (
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	titulo VARCHAR(200) NOT NULL,
+	director VARCHAR(150),
+	duracion INT CHECK (duracion > 0),
+	fecha_publicacion DATE
+);
+
+CREATE TABLE visualizaciones (
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	id_usuario INT NOT NULL,
+	id_pelicula INT NOT NULL,
+	fecha_visualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+	CONSTRAINT fk_visualizaciones_usuario
+		FOREIGN KEY (id_usuario)
+		REFERENCES usuarios(id)
+		ON DELETE CASCADE,
+
+	CONSTRAINT fk_visualizaciones_pelicula
+		FOREIGN KEY (id_pelicula)
+		REFERENCES peliculas(id)
+		ON DELETE CASCADE
+);
